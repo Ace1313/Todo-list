018c7d6c4e02a0f36b126692016df9817aaa4634
Ace1313 <alex.cadjo@hotmail.com>
2022-01-28 17:39:42 +0100

    connected db and setup context

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const { config } = require('dotenv');
 const express = require('express');
+const cors = require('cors');
 require('dotenv/config');
 config();
 
 const { Pool } = require('pg');
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const db = new Pool({
